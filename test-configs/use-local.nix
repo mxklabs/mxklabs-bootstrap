079b78cd3d1f939398e0c424ebeccae638b0164b
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-10-23 13:59:21 +0100

    Amended local.

--- test-configs/use-local.nix
+++ test-configs/use-local.nix
@@ -1,12 +1,13 @@
 let
-  mxklabs = import <mxklabs>;
-in
-  {
-    mxklabs-example-cpp-app = mxklabs.mxklabs-example-cpp-app.overrideAttrs (oldAttrs: {
+  mxklabs-pkgs = import ../../mxklabs-pkgs/default.nix /*<mxklabs>*/ { overlays = [ overlay ]; };
+  overlay = final: prev: {
+    mxklabs-example-cpp-app = prev.mxklabs-example-cpp-app.overrideAttrs (oldAttrs: {
         src = ../../mxklabs-example-cpp-app;
       });
-    mxklabs-example-cpp-lib1 = mxklabs.mxklabs-example-cpp-lib1.overrideAttrs (oldAttrs: {
+    mxklabs-example-cpp-lib1 = prev.mxklabs-example-cpp-lib1.overrideAttrs (oldAttrs: {
         src = ../../mxklabs-example-cpp-lib1;
       });
-  }
+  };
+in
+  mxklabs-pkgs
 
