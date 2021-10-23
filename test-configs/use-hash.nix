688e9a5cd589d12f8ce9ed1314668c32b7dfb334
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-10-23 14:59:58 +0100

    Tweaked test configs

--- test-configs/use-hash.nix
+++ test-configs/use-hash.nix
@@ -1,15 +1,22 @@
 let
-  mxklabs = import <mxklabs>;
-in
-  {
-    mxklabs-example-cpp-app = mxklabs.mxklabs-example-cpp-app.overrideAttrs (oldAttrs: {
-        src = ../../mxklabs-example-cpp-app;
-      });
-    mxklabs-example-cpp-lib1 = mxklabs.mxklabs-example-cpp-lib1.overrideAttrs (oldAttrs: {
+  mxklabs-pkgs = import ../../mxklabs-pkgs/default.nix /*<mxklabs>*/ { overlays = [ overlay ]; };
+  overlay = final: prev: {
+    mxklabs-example-cpp-app = prev.mxklabs-example-cpp-app.overrideAttrs (oldAttrs: {
         src = builtins.fetchGit {
-          url    = "git@github.com:mxklabs/mxklabs-example-cpp-lib1.git";
-          rev    = "efdb78f4529d4892591aab63972fec8bd4fdaf99";
+          url    = "git@github.com:mxklabs/mxklabs-example-cpp-app.git";
+          ref    = "master";
+          rev    = "76e552674864efa2737c38c0b283a611940a3771";
+          #rev    = "55ae05fcc671ad4414317a36fad31c710aa093d7";
         };
       });
-  }
-
+    mxklabs-example-cpp-lib1 = prev.mxklabs-example-cpp-lib1.overrideAttrs (oldAttrs: {
+        src = builtins.fetchGit {
+          url    = "git@github.com:mxklabs/mxklabs-example-cpp-lib1.git";
+          ref    = "master";
+          rev    = "033152d0bd47a97975caaebaf765dd35e11b6b8a";
+          #rev    = "1e4df3ae520734cad9ce6f3d39a06c0c1ba1a03c";
+        };
+      });
+  };
+in
+  mxklabs-pkgs
